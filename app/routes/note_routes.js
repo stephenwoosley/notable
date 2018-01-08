8118cbb8e43bbb8e8de961ec5e004a4dd186b85e
Stephen Woosley <stephenrwoosley@gmail.com>
2018-01-08 13:39:04 -0500

    adding post route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,12 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
-    console.log(req.body);
-    res.send('Changing the message for effect.')
-  })
-}
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
